62932b74824db5f116d7a6cc32cc5e5b97243595
Adriansdls <adrian.s.delasierra@gmail.com>
2022-04-22 20:14:09 +0200

    fix an issue

---
+++ chat1.py
@@ -0,0 +1,3 @@
+pizz = input("Buenas tardes, bienvenido al servicio de pedido online, ¿Cuántas pizzas desea?")
+direcc = input("Estupendo, se están preparando " + pizz + " pizzas. Digame su dirección")
+print("Le mandaremos las ", pizz," pizzas a la dirección" ,direcc,". Muchas gracias or su pedido.")
